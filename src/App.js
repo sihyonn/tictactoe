c73951f57c9a4bf8b88609e0eae61db65e8b1483
sihyonn <sihyeon982@naver.com>
2023-08-30 12:30:35 +0900

    feat: 동작에 대한 기록 저장을 위해 App 컴포넌트로 옮겨주기

--- src/App.js
+++ src/App.js
@@ -1,14 +1,74 @@
+import { useState } from "react";
 import "./App.css";
 import Board from "./components/Board";
 
 function App() {
+  const [history, setHistory] = useState([{ squares: Array(9).fill(null) }]);
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const calcWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let idx = 0; idx < lines.length; idx++) {
+      const [a, b, c] = lines[idx];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  };
+
+  const current = history[history.length - 1];
+  const winner = calcWinner(current.squares);
+
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = `Next player: ${xIsNext ? "X" : "O"}`;
+  }
+
+  const handleClick = (i) => {
+    const newSquares = current.squares.slice();
+    if (calcWinner(newSquares) || newSquares[i]) {
+      return;
+    }
+    newSquares[i] = xIsNext ? "X" : "O";
+    setHistory([...history, { squares: newSquares }]);
+    setXIsNext((current) => !current);
+  };
+
+  const moves = history.map((step, move) => {
+    const desc = move ? "#" + move + "로 이동" : "게임 시작하기";
+    return (
+      <li key={move}>
+        <button>{desc}</button>
+      </li>
+    );
+  });
+
   return (
     <div className="App">
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board squares={current.squares} onClick={(i) => handleClick(i)} />
         </div>
-        <div className="game-info"></div>
+        <div className="game-info">
+          <div className="status">{status}</div>
+          <ol className="moves">{moves}</ol>
+        </div>
       </div>
     </div>
   );
